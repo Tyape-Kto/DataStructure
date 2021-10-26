4e8d2f7c543e9bf6776c194986939ff5c16b87d3
Tyape-Kto <55008900+Tyape-Kto@users.noreply.github.com>
2021-10-27 00:24:02 +0545

    Update Binary_Bisection_Search.py

--- Binary_Bisection_Search.py
+++ Binary_Bisection_Search.py
@@ -1,4 +1,6 @@
 # this is linear or Bisection searching algorithm otherwise known as binary search, stupid people
 # see linear search is like if you have a book with 105 pages and you have to find 104th page, linear search will go for 53 turns to find that pages from start.
 # linear search - going through each to match one thing you are looking for, if 1000000th email then 1000000 times you have to search, stupid 
-# if you have certain information like number sorted or email address arranged in sorted order you can use it, and your time complexity changes from O(n) of 1000000 steps to O(log(n)) of 23 steps only.
+# if you have certain information like number sorted or email address arranged in sorted order you can use it, 
+                         # and your time complexity changes from O(n) of 10,000,000 steps to O(log(n)) of 23 steps only.
+  
